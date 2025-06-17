69005f1cbb4df46e09c29931b0512dbda3090710
kadersahib <kadermaraikar99@gmail.com>
2025-06-17 20:19:07 +0530

    change

--- js/fotter.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch('component/fotter.html')
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector('.footer-container').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading footer:', error));
-});
